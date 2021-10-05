4c6eaf253b5d343539ad699730e06815034a705c
hemananthaws <81817351+hemananthaws@users.noreply.github.com>
2021-10-05 16:54:25 +0530

    Create variables.tf

---
+++ Hemananth/GKE-terraform/variables.tf
@@ -0,0 +1,16 @@
+
+variable "name" {
+  default = "my-cluster"
+}
+variable "project" {
+  default = "absolute-realm-322617"
+}
+variable "location" {
+  default = "us-central1"
+}
+variable "initial_node_count" {
+  default = 1
+}
+variable "machine_type" {
+  default = "e2-medium"
+}
