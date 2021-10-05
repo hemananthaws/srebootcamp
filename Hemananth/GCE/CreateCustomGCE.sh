0dc1dfb954836fb5cdf3c1c9111e5f782d5f825b
hemananthaws <81817351+hemananthaws@users.noreply.github.com>
2021-10-05 16:58:55 +0530

    Create CreateCustomGCE.sh

---
+++ Hemananth/GCE/CreateCustomGCE.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+export ZONE="us-central1-a"
+echo "Please enter instance Name?"
+read INSTANCE_NAME
+gcloud compute instances create $INSTANCE_NAME \
+        --zone=$ZONE --machine-type=e2-medium --subnet=default \
+        --tags=http-server,https-server \
+        --image=centos-7-v20210817 --image-project=centos-cloud \
+        --boot-disk-size=20GB --boot-disk-type=pd-balanced \
+        --boot-disk-device-name=$INSTANCE_NAME
+~                                              
