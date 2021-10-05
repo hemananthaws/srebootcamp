75a3bc6e71896808cb63bc9fcb9c3bf274057eda
hemananthaws <81817351+hemananthaws@users.noreply.github.com>
2021-10-05 16:45:24 +0530

    Update CreateGKE.sh

--- Hemananth/GKE/CreateGKE.sh
+++ Hemananth/GKE/CreateGKE.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 echo "Please Enter the Cluster Name to create?"
 read Name
-echo "Please Enter the Zone for Cluster $Name?"
-read ZONE
 gcloud container clusters create $Name \
-        --zone=$ZONE \
+        --zone=us-central1-a \
         --num-nodes=2 \
         --enable-autoscaling --max-nodes=5 --min-nodes=2 \
         --addons HorizontalPodAutoscaling,HttpLoadBalancing \
         --enable-autorepair \
+kubectl apply -f Deployment.yaml
+
